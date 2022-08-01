f8c367ed245c12e4b88b0d93b172636272d60efa
IngridMorg <sc20iecm@leeds.ac.uk>
2022-08-01 16:24:21 +0100

    completed the manual approach to the text cleaning lesson

---
+++ cleanData.py
@@ -0,0 +1,11 @@
+#lesson 2
+#https://machinelearningmastery.com/crash-course-deep-learning-natural-language-processing/
+
+import nltk
+
+#nltk approach
+
+
+
+
+
---
+++ cleanDataManual.py
@@ -0,0 +1,35 @@
+#lesson 2
+#https://machinelearningmastery.com/crash-course-deep-learning-natural-language-processing/
+
+#this is the manual approach to word tokenisation
+import string
+#open file and read text into the variable 'text
+filename = 'data/metamorphosis.txt'
+file = open(filename,encoding="utf-8")
+
+text = file.read()
+file.close()
+
+#split the words based on white space
+words = text.split()
+
+#make everything lower case
+words = [w.lower() for w in words]
+#remove punctuation
+translator = str.maketrans('','',string.punctuation)
+for i in range(0,100):
+    words[i] = words[i].translate(str.maketrans('','',string.punctuation))
+    #we have to use this section as the downloaded text uses symbols not contained within string.punctuation
+    words[i] = words[i].replace('“','')
+    words[i] = words[i].replace('’', '')
+    words[i] = words[i].replace('”','')
+#i think im going to leave in numbers, they can be useful
+
+for i in range(0,100):
+    print(words[i])
+
+
+phrase = "hello, this is a \"phrase\" containing many \'different\' punctuation%$\"\" types"
+#print(phrase)
+phrase = phrase.translate(str.maketrans('','',string.punctuation))
+#print(phrase)
