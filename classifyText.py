9a82e80475bbc83476365001904179164de9e3e8
IngridMorg <sc20iecm@leeds.ac.uk>
2022-08-03 15:31:22 +0100

    classification research done elsewhere

---
+++ classifyText.py
@@ -0,0 +1,16 @@
+#https://machinelearningmastery.com/crash-course-deep-learning-natural-language-processing/
+
+#CNN = Convolutional Neural Network
+
+#the arcitecture this lesson follows is as such
+    #word embedding model:
+        # simple distrubuted representation of words where different words that have a similar meaning also have a similar
+        #representation
+    #convolutional model:
+        # a feature extraction model that learns to extract salient features from documents represented using a word embedding
+    #fully-connected model:
+        #the intepretation of extracted features in terms of a predicted output
+
+#a kernel is a filter that is used to extract the features from the images
+#hyperparameters = parameters whose values control the learning process and determine the values of model parameters that
+#a learning algorithm ends up learning
