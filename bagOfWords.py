8945e6b0aac37e49ad5a35fe03dbb6a31bce0f36
IngridMorg <sc20iecm@leeds.ac.uk>
2022-08-03 11:51:35 +0100

    finished the keras bag-of-words appraoch and did substantial further reading on this approach, as seen by the comments in the functions code

--- bagOfWords.py
+++ bagOfWords.py
@@ -3,6 +3,7 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 #we need to load and tokenise the text
 from cleanDataManual import *
+from keras.preprocessing.text import Tokenizer
 def loadText(file):
     # open file and read text into the variable 'text
     file = open(file, encoding="utf-8")
@@ -10,7 +11,9 @@ def loadText(file):
     text = file.read()
     file.close()
     # split the words based on newline characters
-    text = text.split("\n")
+    text = text.splitlines()
+    for i in range(0,len(text)):
+        text[i] = text[i].translate(str.maketrans('', '', string.punctuation))
     return text
 
 def scikitBag(file):
@@ -30,15 +33,59 @@ def scikitBag(file):
     vectorizer.fit(text)
     #summarise the vocab
     print(vectorizer.vocabulary_)
-    #print(vectorizer.idf_)
+    print(vectorizer.idf_)
     #now we encode the document
     vector = vectorizer.transform([text[0]])
     #summarise the encoded vector
-    #print(vector.shape)
-    #print(vector.toarray())
-    #print(vector)
+    print(vector.shape)
+    print(vector.toarray())
+    print(vector)
+    return vector
 
 
-scikitBag("data/damonText.txt")
 
-#def kerasBag():
+def kerasBag(file):
+    #in keras bag of words is used through the Tokenizer object
+    #tokenizer allows us to vectorise a text corpus bu turning each text into either a
+    #sequence of integers or into a vector(where the coefficient for each token can be binary, based on ocurrence or tf-idf etc
+    #by defaul all punctuation is removed, but words may contain '
+        #these sequences are then split into lists of tokens
+        #0 is a reserved index that wont be assigned to any word
+    # a sequence is a list of integer word indices
+    # a text can be a list of strings, generator of strings or a list of lists of strings
+    #we can also get the configuration that the tokenizer is using (get_config) which returns a python dictionary with
+    #the tokenizers confuguration
+    #sequences_to_matrix converts a list of sequences (integer word indices) to a numpy matrix
+    #sequences_to_texts, transforms each sequence to a list of text
+        #only the top (num_of_words -1) will be taken into account
+        #sequences_to_texts generator, transforms each sequence in a list of sequences to a list of texts(strings)
+    #texts_to_matrix, converts a list of texts to a numpy matrix
+
+    text = loadText(file)
+
+    t = Tokenizer()
+    #fit tokeizer to documents
+    t.fit_on_texts(text)
+    #summarise
+    #this is  a dictionary of words and their counts
+    print(t.word_counts)
+    #this is a dictionary of words and how many documents they appeared in
+    print(t.document_count)
+    #this is a dictioanry of words and their uniquely assigned integers
+    print(t.word_index)
+    #this is an integer count of the total number of documents that were used to fit the tokenizer
+    print(t.word_docs)
+
+    #integer encode the documents (one hot encoding in this case i believe)
+    #one hot encoding does not car about word order, it loses the context we often need
+    #but is still a very successful approach
+    encoded_docs = t.texts_to_matrix(text, mode='count')
+    print(encoded_docs)
+
+    #once we have a tokenizer that we have fit on training data we can use it to encode documents in train or test datasets
+
+
+    return t, encoded_docs
+
+#scikitBag("data/damonText.txt")
+kerasBag("data/damonText.txt")
